69df84867bece034941abe73b40a0e152f6f6336
Paul_GD <paulskapczyk@gmail.com>
2021-03-29 19:00:25 +0200

    Create rgb_table.h

---
+++ rgb/rgb_table.h
@@ -0,0 +1,104 @@
+/*
+ * RGB LED assignment table for the eVision VS12L03A
+ * Configuration for the Glorious GMMK TKL (ISO), 2020 variant with USB-C
+ *
+ * Copyright (c) 2021 Paul_GD and Sono
+ * All Rights Reserved.
+*/
+
+// Controller 0xEE
+#define KC_LSFT_RGB 0x03
+#define KC_NUBS_RGB 0x04
+#define KC_Z_RGB 0x05
+#define KC_X_RGB 0x06
+#define KC_C_RGB 0x07
+#define KC_V_RGB 0x08
+#define KC_B_RGB 0x09
+#define KC_N_RGB 0x0A
+#define KC_M_RGB 0x0B
+#define KC_COMM_RGB 0x0C
+#define KC_DOT_RGB 0x0D
+#define KC_SLSH_RGB 0x0E
+#define KC_RSFT_RGB 0x0F
+//
+#define KC_CAPS_RGB 0c30
+#define KC_A_RGB 0x31
+#define KC_S_RGB 0x32
+#define KC_D_RGB 0x36
+#define KC_F_RGB 0x37
+#define KC_G_RGB 0x38
+#define KC_H_RGB 0x39
+#define KC_J_RGB 0x3A
+#define KC_K_RGB 0x3B
+#define KC_L_RGB 0x3C
+#define KC_SCLN_RGB 0x3D
+#define KC_QUOT_RGB 0x3E
+#define KC_ENT_RGB 0x3F
+//
+#define KC_TAB_RGB 0x60
+#define KC_Q_RGB 0x61
+#define KC_W_RGB 0x62
+#define KC_E_RGB 0x63
+#define KC_R_RGB 0x64
+#define KC_T_RGB 0x65
+#define KC_Y_RGB 0x69
+#define KC_U_RGB 0x6A
+#define KC_I_RGB 0x6B
+#define KC_O_RGB 0x6C
+#define KC_P_RGB 0x6D
+#define KC_LBRC_RGB 0x6E
+#define KC_RBRC_RGB 0x6F
+//
+#define KC_GRV_RGB 0x90
+#define KC_1_RGB 0x91
+#define KC_2_RGB 0x92
+#define KC_3_RGB 0x93
+#define KC_4_RGB 0x94
+#define KC_5_RGB 0x95
+#define KC_6_RGB 0x96
+#define KC_7_RGB 0x97
+#define KC_8_RGB 0x98
+#define KC_9_RGB 0x9C
+#define KC_0_RGB 0x9D
+#define KC_MINS_RGB 0x9E
+#define KC_EQL_RGB 0x9F
+// 
+#define KC_ESC_RGB 0xC0
+#define KC_F1_RGB 0xC1
+#define KC_F2_RGB 0xC2
+#define KC_F3_RGB 0xC3
+#define KC_F4_RGB 0xC4
+#define KC_F5_RGB 0xC5
+#define KC_F6_RGB 0xC6
+#define KC_F7_RGB 0xC7
+#define KC_F8_RGB 0xC8
+#define KC_F9_RGB 0xC9
+#define KC_F10_RGB 0xCA
+
+
+// Controller E8
+#define KC_LCTL 0x03
+#define KC_LGUI 0x04
+#define KC_LALT 0x05
+#define KC_SPC 0x07
+#define KC_RALT 0x09
+#define FN 0x0A
+#define KC_APP 0x0B
+#define KC_RCTL 0x0D
+#define KC_LEFT 0x0E
+#define KC_DOWN 0x0F
+#define KC_UP 0x36
+#define KC_RGHT 0x3B
+#define KC_END 0x60
+#define KC_PGDN 0x61
+#define KC_INS 0x90
+#define KC_HOME 0x91
+#define KC_PGUP 0x92
+#define KC_NUHS 0x97
+#define KC_DEL 0x98
+#define KC_F11 0xC0
+#define KC_F12 0xC1
+#define KC_PSCR 0xC2
+#define KC_SLCK 0xC3
+#define KC_PAUS 0xC4
+#define KC_BSPC 0xCA
